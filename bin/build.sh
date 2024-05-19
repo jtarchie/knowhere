03cb0306565331758629b824b013cb6e97e2ed75
JT Archie <jtarchie@gmail.com>
2024-05-19 06:35:47 -0600

    new build for countries and states

--- bin/build.sh
+++ bin/build.sh
@@ -4,32 +4,32 @@ set -eux
 
 mkdir -p .build/
 
-states=("alabama" "alaska" "arizona" "arkansas" "california" "colorado" "connecticut" "delaware" "florida" "georgia" "hawaii" "idaho" "illinois" "indiana" "iowa" "kansas" "kentucky" "louisiana" "maine" "maryland" "massachusetts" "michigan" "minnesota" "mississippi" "missouri" "montana" "nebraska" "nevada" "new-hampshire" "new-jersey" "new-mexico" "new-york" "north-carolina" "north-dakota" "ohio" "oklahoma" "oregon" "pennsylvania" "rhode-island" "south-carolina" "south-dakota" "tennessee" "texas" "utah" "vermont" "virginia" "washington" "west-virginia" "wisconsin" "wyoming")
-
 db_path=.build/entries.db
 
 rm -Rf "$db_path"*
 
-for state in "${states[@]}"; do
-	filename="$state.osm.pbf"
+while read -r url; do
+	filename="$(basename "$url")"
+	country_code="$(basename "$url" | awk -F'[/-]' '{print $(NF-1)}')"
+
 	pushd .build/
 	if [ ! -f "$filename" ]; then
-		curl -o "$filename" "https://download.geofabrik.de/north-america/us/$state-latest.osm.pbf"
+		curl -o "$filename" "$url"
 	fi
 	popd
 
 	go run -tags fts5 github.com/jtarchie/knowhere build \
 		--osm ".build/$filename" \
 		--db "$db_path" \
-		--prefix "$state" --allowed-tags "name"
-done
+		--prefix "$country_code" --allowed-tags "name"
+done <"$1"
 
 go run github.com/SaveTheRbtz/zstd-seekable-format-go/cmd/zstdseek \
 	-f "$db_path" \
 	-o "$db_path".zst \
 	-q 7
 
-rclone copy "$db_path" r2:knowhere-sqlite/ -P
-rclone copy "$db_path".zst r2:knowhere-sqlite/ -P
+# rclone copy "$db_path" r2:knowhere-sqlite/ -P
+# rclone copy "$db_path".zst r2:knowhere-sqlite/ -P
 
-./bin/cleanup.sh
+# ./bin/cleanup.sh
