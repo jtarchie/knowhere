5816eb20b9f7ac160072541a425a0e6283590cdc
JT Archie <jtarchie@gmail.com>
2024-06-24 10:56:45 -0600

    extract builds tags into file

--- bin/build.sh
+++ bin/build.sh
@@ -37,7 +37,7 @@ done
 go run -tags fts5 github.com/jtarchie/knowhere build \
 	--config "$config_path" \
 	--db "$db_path" \
-	--allowed-tags "name,amenity,shop,leisure,tourism,boundary,admin_level,waterway,border_type"
+	--allowed-tags "$(grep -v '^#' bin/tags | tr '\n' ',')"
 
 # Compressing the database
 go run github.com/SaveTheRbtz/zstd-seekable-format-go/cmd/zstdseek \
