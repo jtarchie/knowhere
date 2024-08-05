241ac6fdaa5bc3ac11144effb0e05297a1a86793
JT Archie <jtarchie@gmail.com>
2024-08-05 08:19:15 -0600

    add geohash

--- query/indexed_sql.go
+++ query/indexed_sql.go
@@ -5,6 +5,9 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/mmcloughlin/geohash"
+	"github.com/paulmach/orb"
+	"github.com/paulmach/orb/geo"
 	"github.com/samber/lo"
 )
 
@@ -172,6 +175,40 @@ func ToIndexedSQL(query string) (string, error) {
 		}
 	}
 
+	if bbs, ok := ast.Directives["bb"]; ok {
+		if 4 == len(bbs) {
+			parts = append(
+				parts,
+				bbs[0]+" <= s.minLon",
+				bbs[1]+" <= s.minLat",
+				"s.maxLon <= "+bbs[2],
+				"s.maxLat <= "+bbs[3],
+			)
+
+			// find distance of lat/lng
+			bounds := orb.Bound{
+				Min: orb.Point{toFloat(bbs[0]), toFloat(bbs[1])},
+				Max: orb.Point{toFloat(bbs[2]), toFloat(bbs[3])},
+			}
+			precision := boundsToGeohashPrecision(bounds)
+			center := bounds.Center()
+
+			// get all neighboring hashes
+			hash := geohash.EncodeWithPrecision(center.Lat(), center.Lon(), precision)
+			hashes := []string{hash}
+			hashes = append(hashes, geohash.Neighbors(hash)...)
+
+			// add to full text search
+			asString := lo.Map(hashes, func(item string, _ int) string {
+				return item + `*`
+			})
+			equalParts = append(
+				equalParts,
+				`( "geohash" AND ( `+strings.Join(asString, " OR ")+" ) )",
+			)
+		}
+	}
+
 	if 0 < len(equalParts) {
 		equals := strings.Join(equalParts, " AND ")
 		if 0 < len(notParts) {
@@ -190,18 +227,6 @@ func ToIndexedSQL(query string) (string, error) {
 		)
 	}
 
-	if bbs, ok := ast.Directives["bb"]; ok {
-		if 4 == len(bbs) {
-			parts = append(
-				parts,
-				bbs[0]+" <= s.minLon",
-				bbs[1]+" <= s.minLat",
-				"s.maxLon <= "+bbs[2],
-				"s.maxLat <= "+bbs[3],
-			)
-		}
-	}
-
 	for index, part := range parts {
 		if 0 < index {
 			builder.WriteString(" AND ")
@@ -209,4 +234,37 @@ func ToIndexedSQL(query string) (string, error) {
 		builder.WriteString(part)
 	}
 	return builder.String(), nil
+}
+
+func toFloat(s string) float64 {
+	value, _ := strconv.ParseFloat(s, 64)
+	return value
+}
+
+func boundsToGeohashPrecision(bounds orb.Bound) uint {
+	geohashPrecisions := [][2]float64{
+		{0.0372, 0.0186},
+		{0.149, 0.149},
+		{1.19, 0.596},
+		{4.77, 4.77},
+		{38.2, 19.1},
+		{153, 153},
+		{1220, 610},
+		{4890, 4890},
+		{39100, 19500},
+		{156000, 156000},
+		{1250000, 625000},
+		{5000000, 5000000},
+	}
+
+	height := geo.BoundHeight(bounds)
+	width := geo.BoundWidth(bounds)
+
+	for index, precision := range geohashPrecisions {
+		if height <= precision[0] && width <= precision[1] {
+			return uint(len(geohashPrecisions) - index + 1)
+		}
+	}
+
+	return uint(len(geohashPrecisions))
 }
