0749fba55ebd75e2958967c6dfbb82c437cd3bf5
JT Archie <jtarchie@gmail.com>
2024-08-29 12:30:52 -0600

    add cli info

--- commands/cli.go
+++ commands/cli.go
@@ -3,11 +3,11 @@ package commands
 import "log/slog"
 
 type CLI struct {
-	Build     Build      `cmd:""`
-	Convert   Convert    `cmd:""`
-	Generate  Generate   `cmd:""`
-	Integrity Integrity  `cmd:"" help:"Check integrity between uncompressed and compressed databases"`
-	Runtime   Runtime    `cmd:""`
-	Server    Server     `cmd:""`
+	Build     Build      `cmd:"" help:"builds database by downloading PBF"`
+	Convert   Convert    `cmd:"" help:"convert PBF to sqlite"`
+	Generate  Generate   `cmd:"" help:"generate a query to sql (helpful for piping into sqlite)"`
+	Integrity Integrity  `cmd:"" help:"check integrity between uncompressed and compressed databases"`
+	Runtime   Runtime    `cmd:"" help:"run javascript files locally"`
+	Server    Server     `cmd:"" help:"start the API server"`
 	LogLevel  slog.Level `help:"Set the log level (debug, info, warn, error)" default:"info"`
 }
