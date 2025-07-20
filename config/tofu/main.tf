727b228d0e781b5579189291e7844f485ea2a0d9
JT Archie <jtarchie@gmail.com>
2025-07-19 22:41:48 -0600

    update firewall rules

--- config/tofu/main.tf
+++ config/tofu/main.tf
@@ -1,17 +1,46 @@
 resource "digitalocean_droplet" "web" {
-  name      = "kamal-web"
-  image     = "docker-20-04"
-  region    = "nyc1"
-  size      = "s-1vcpu-1gb"
-  ssh_keys  = [46054806]
-  user_data = <<-USER_DATA
-    #!/bin/bash
-    sudo ufw insert 1 allow from ${var.ip_allowlist} to any port 22
-    sudo ufw insert 2 allow port 8080
-    sudo ufw reload
-  USER_DATA
+  name     = "kamal-web"
+  image    = "docker-20-04"
+  region   = "nyc1"
+  size     = "s-1vcpu-1gb"
+  ssh_keys = [46054806]
+}
+
+resource "digitalocean_firewall" "web" {
+  name = "kamal-web-firewall"
+
+  droplet_ids = [digitalocean_droplet.web.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
 }
 
 output "droplet_ip_address" {
-  value = digitalocean_droplet.web.*.ipv4_address
+  value = digitalocean_droplet.web.ipv4_address
 }
