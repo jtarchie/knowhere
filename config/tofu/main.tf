78f7b181acd44a4e4175caf9570f424bde19e375
JT Archie <jtarchie@gmail.com>
2025-07-22 06:19:41 -0600

    enable tls port

--- config/tofu/main.tf
+++ config/tofu/main.tf
@@ -23,6 +23,12 @@ resource "digitalocean_firewall" "web" {
     source_addresses = ["0.0.0.0/0", "::/0"]
   }
 
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
   outbound_rule {
     protocol              = "tcp"
     port_range            = "1-65535"
