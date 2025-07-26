c1ccf7173dcc2fb8218c54146d5c00de58336b16
JT Archie <jtarchie@gmail.com>
2025-07-26 07:51:41 -0600

    increase timeout

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -4,4 +4,4 @@ set -eux
 
 filename="entries.db.zst"
 
-/app/knowhere server --port 3000 --log-level debug --db /var/osm/$filename --cors "${CORS_DOMAIN:-*}" --allow-cidr "${ALLOW_CIDR:-0.0.0.0/0}" --runtime-timeout 10s
+/app/knowhere server --port 3000 --log-level debug --db /var/osm/$filename --cors "${CORS_DOMAIN:-*}" --allow-cidr "${ALLOW_CIDR:-0.0.0.0/0}" --runtime-timeout 30s
